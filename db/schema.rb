06acd8930595a38dd7f9354e28976afbe04a7670
Jonny Gates <jonny.gates@outlook.com>
2019-02-21 11:44:43 +0000

    Added validations to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_111539) do
+ActiveRecord::Schema.define(version: 2019_02_21_114200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_111539) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id", "cocktail_id"], name: "index_doses_on_ingredient_id_and_cocktail_id", unique: true
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
