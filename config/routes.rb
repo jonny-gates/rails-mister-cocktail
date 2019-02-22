24cfb5be3f22213f9dc85e8e8861e690f6799843
Jonny Gates <jonny.gates@outlook.com>
2019-02-22 14:57:16 +0000

    Create reviews routes'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "cocktails#index"
   resources :cocktails, only: [:show, :new, :create, :edit, :update] do
     resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end
